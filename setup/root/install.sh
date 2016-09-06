42afed007231c02953b4b5c99084b92f4b952295
binhex <megalith01@gmail.com>
2016-09-06 21:48:38 +0100

    initial punt

---
+++ setup/root/install.sh
@@ -0,0 +1,19 @@
+#!/bin/bash
+
+# exit script if return code != 0
+set -e
+
+# define pacman packages
+pacman_packages="base-devel git python2-pyopenssl unzip unrar"
+
+# install pre-reqs
+pacman -S --needed $pacman_packages --noconfirm
+
+# call aur install script (arch user repo)
+source /root/aur.sh
+
+# cleanup
+yes|pacman -Scc
+rm -rf /usr/share/locale/*
+rm -rf /usr/share/man/*
+rm -rf /tmp/*
